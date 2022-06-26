f6aeaf654568824dcece1f3772fc397631a6e103
Ryang Sohn <loop.infinitely@gmail.com>
2022-06-26 15:04:31 +0900

    Add card removal and edit stub

--- frontend/src/card-list/card-list.tsx
+++ frontend/src/card-list/card-list.tsx
@@ -10,32 +10,51 @@ export default function CardList() {
   React.useEffect(() => {
     async function fetchCards() {
       const res = await client.get<CardEntity[]>("/cards");
-      console.log(res);
       setCards(res.data);
     }
     fetchCards();
   }, []);
+
+  const removeCard = async (id: number) => {
+    const newCards = cards.filter((card) => card.id != id);
+    setCards(newCards);
+    await client.delete(`/cards/${id}`);
+  };
+
+  const editCard = (id: number) => {
+    console.log(`Edit card ${id}`);
+  };
 
   return (
     <>
       <main>
         <h2>카드 리스트</h2>
         <div className="cardContainer">
-          {cards.map((card) => (
-            <Card
-              id={card.id}
-              label={card.label}
-              term={card.term}
-              imageUrl={`${process.env.API_SERVER}/images/${card.image?.id}`}
-              editEnabled={true}
-            />
-          ))}
+          {cards.length > 0 ? (
+            cards.map((card) => (
+              <Card
+                key={card.id}
+                id={card.id}
+                label={card.label}
+                term={card.term}
+                imageUrl={`${process.env.API_SERVER}/images/${card.image?.id}`}
+                editEnabled={true}
+                editCard={editCard}
+                removeCard={removeCard}
+              />
+            ))
+          ) : (
+            <p>카드가 없습니다.</p>
+          )}
         </div>
       </main>
       <nav>
         <Link to="/create-card">카드 추가</Link>
         <br />
         <Link to="/learn">학습 시작</Link>
+        <br />
+        <br />
+        <Link to="/">Home</Link>
       </nav>
     </>
   );
